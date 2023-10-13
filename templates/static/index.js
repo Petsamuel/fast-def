14071d52f4557993ebcee3865ed044b52fcd25a4
petsamuel <petsamuel4@gmail.com>
2023-10-13 23:59:39 +0100

    initial

--- templates/static/index.js
+++ templates/static/index.js
@@ -6,3 +6,20 @@ document.addEventListener("DOMContentLoaded", function () {
     audioPlayer.play();
   });
 });
+
+var today = new Date();
+
+// Format the date as needed
+var formattedDate =
+  today.getFullYear() +
+  "-" +
+  (today.getMonth() + 1).toString().padStart(2, "0") +
+  "-" +
+  today.getDate().toString().padStart(2, "0");
+
+// Display the date using jQuery
+jQuery(document).ready(function ($) {
+  $("#dateDisplay").text("Today's Date: " + formattedDate);
+  console.log(formattedDate);
+
+});
