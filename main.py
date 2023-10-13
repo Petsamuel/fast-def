b603a124c0f8b808c8f1f7d7f44e3e59401ea142
petsamuel <petsamuel4@gmail.com>
2023-10-13 23:29:23 +0100

    initial

--- main.py
+++ main.py
@@ -13,27 +13,43 @@ app.mount("/templates/static", StaticFiles(directory="templates"), name="static"
 api_url = "http://api.dictionaryapi.dev/api/v2/entries/en/"
 
 def get_definition(word: str):
-    response = requests.get(api_url + word)
-    if response.status_code == 200:
+    try:
+        response = requests.get(api_url + word)
+        response.raise_for_status()  # Raise an exception if the response status is not 2xx
         return response.json()
-    else:
+    except requests.exceptions.RequestException as e:
+        # Handle the request error gracefully
         return {"error": "Word not found or API request failed"}
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    default_word = "welcome"  # Set a default word to fetch data on load
+    data = get_definition(default_word)
+    
+    if data is None:
+        raise HTTPException(status_code=404, detail="Word not found or API request failed")
+    
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
     
 @app.get("/word/{word}", response_class=HTMLResponse)
 async def word(request: Request, word: str):
     if word == "":
-        return templates.TemplateResponse("error.html", {"request": request, "error": "Word is required"})
-    else:
-        data = get_definition(word)
-        return templates.TemplateResponse("index.html", {"request": request, "data": data})
+        raise HTTPException(status_code=400, detail="Word is required")
+    
+    data = get_definition(word)
+    
+    if data is None:
+        raise HTTPException(status_code=404, detail="Word not found or API request failed")
+    
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
 
 @app.post("/word", response_class=HTMLResponse)
 async def word(request: Request, word: str = Form(...)):
     data = get_definition(word)
+    
+    if data is None:
+        raise HTTPException(status_code=404, detail="Word not found or API request failed")
+    
     return templates.TemplateResponse("index.html", {"request": request, "data": data})
 
 
--- templates/static/index.js
+++ templates/static/index.js
@@ -6,5 +6,3 @@ document.addEventListener("DOMContentLoaded", function () {
     audioPlayer.play();
   });
 });
-
-alert("testing..");
