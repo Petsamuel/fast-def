55a42f497f86f74ccb3c97a3cbb2cd2b478b358a
petsamuel <petsamuel4@gmail.com>
2023-10-14 02:22:25 +0100

    initial

--- templates/static/index.js
+++ templates/static/index.js
@@ -10,16 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
 var today = new Date();
 
 // Format the date as needed
-var formattedDate =
-  today.getFullYear() +
-  "-" +
-  (today.getMonth() + 1).toString().padStart(2, "0") +
-  "-" +
-  today.getDate().toString().padStart(2, "0");
+var formattedDate = today.getFullYear();
 
 // Display the date using jQuery
-jQuery(document).ready(function ($) {
-  $("#dateDisplay").text("Today's Date: " + formattedDate);
-  console.log(formattedDate);
-
-});
+$("#dateDisplay").text("Today's Date: " + formattedDate);
