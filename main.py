b5808cdbe3570c1597b2534db48271f53f2a3fad
petsamuel <petsamuel4@gmail.com>
2023-10-14 01:22:59 +0100

    initial

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# from pydantic import BaseModel
 from fastapi import FastAPI, Request, HTTPException, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from starlette.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import requests
-
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -14,16 +12,16 @@ api_url = "http://api.dictionaryapi.dev/api/v2/entries/en/"
 
 def get_definition(word: str):
     try:
-        response = requests.get(api_url + word)
+        response = requests.get(api_url + word.lower())  # Convert word to lowercase
         response.raise_for_status()  # Raise an exception if the response status is not 2xx
         return response.json()
     except requests.exceptions.RequestException as e:
         # Handle the request error gracefully
-        return {"error": "Word not found or API request failed"}
+        return None
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    default_word = "welcome"  # Set a default word to fetch data on load
+    default_word = "example"  # Set a default word to fetch data on load
     data = get_definition(default_word)
     
     if data is None:
@@ -39,7 +37,7 @@ async def word(request: Request, word: str):
     data = get_definition(word)
     
     if data is None:
-        raise HTTPException(status_code=404, detail="Word not found or API request failed")
+        return RedirectResponse(url="/", status_code=302)  # Redirect to the home page
     
     return templates.TemplateResponse("index.html", {"request": request, "data": data})
 
@@ -48,11 +46,11 @@ async def word(request: Request, word: str = Form(...)):
     data = get_definition(word)
     
     if data is None:
-        raise HTTPException(status_code=404, detail="Word not found or API request failed")
+        return RedirectResponse(url="/", status_code=302)  # Redirect to the home page
     
     return templates.TemplateResponse("index.html", {"request": request, "data": data})
 
-
-
-    
-
+# Catch-all route to handle invalid endpoints
+@app.get("/{path:path}", response_class=RedirectResponse)
+async def catch_all(path: str):
+    return RedirectResponse(url="/", status_code=302)
